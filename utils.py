a2cc03bb7e8bd0036d5c7f8eac23f64c500a6e61
AndrewWant <42478214+AndrewWant@users.noreply.github.com>
2022-04-19 13:37:33 +0200

    Update utils.py
    Added function for generating save filepath

--- utils.py
+++ utils.py
@@ -1,1 +1,24 @@
 # Utils file for generally useful functions, etc.
+
+import os
+from datetime import datetime
+
+def generate_output_filename(filename_stub,
+                             location=""
+                             ):
+    """
+    Creates an output filename for saving plots, allowing systematic saving of plots/images from analysis.
+    :param filename_stub:
+    :param location:
+    :return:
+    """
+    if isinstance(location, str):
+        if (location == "") or not os.path.exists(location):
+            location = os.getcwd()
+    else:
+        print("Invalid argument: {} supplied for location. Only strings allowed.".format(location))    
+        
+    today_string = datetime.today().strftime("%Y-%m-%d_%H_%M")
+    return os.sep.join((location, "_".join((today_string, filename_stub))))
+  
+  
