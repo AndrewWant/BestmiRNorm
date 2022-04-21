731818822b95ca2be5a7efa806b566f1da2cea75
AndrewWant <42478214+AndrewWant@users.noreply.github.com>
2022-04-21 12:26:27 +0200

    Update find_normalisers.py

--- find_normalisers.py
+++ find_normalisers.py
@@ -1,3 +1,14 @@
 from . import miRNA_normalisers as miR_norm
+from . import check_input_files as chk_files
 
+xl_file = miR_norm.get_filename()
+sheet_name = input("Enter Sheet Name with Your Data: ")
+normaliser_file = miR_norm.get_filename("normaliser")
+
+normalisers = miR_norm.get_candidate_normalisers(normaliser_file)
+
+if miR_norm.check_xl_file(xl_file, sheet_name, normaliser_file):
+    pass
+else:
+    break
 
